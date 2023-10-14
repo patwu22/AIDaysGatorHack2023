a76c7a84833eba79b3fb6ea81ba0b3a52235f729
Ruobin Chen <chenrbin04@gmail.com>
2023-10-14 19:31:57 -0400

    Testing prompts 2 and 3

--- src/CategoriesStack.js
+++ src/CategoriesStack.js
@@ -15,9 +15,10 @@ function CategoriesStack() {
             const response = await axios.post(
                 "https://api.openai.com/v1/completions",
                 {
-                    prompt: "Generate the names of three broad categories of goods or services. They should be broad enough to contain three subcategories. In your response, only provide the name of each broad category, separated by comma.No formatting.",
+                    prompt: 'Generate the names of three broad categories of goods or services.They should be broad enough to contain three subcategories.Each item should have its first letter capitalized.In your response, only provide the name of each broad category, separated by comma.Do not include any words other than the categories.Example: “Food, Arts and Crafts Materials, Tech Gadgets”',
                     model: 'text-davinci-002',
                     max_tokens: 50,
+                    temperature: 0.5,
                     n: 1,
                     stop: ".",
                 },
@@ -34,45 +35,53 @@ function CategoriesStack() {
             setc3(cate[2]);
         }
         
-        //async function OpenaiPrompt2(name) {
-        //    const response = await axios.post(
-        //        "https://api.openai.com/v1/completions",
-        //        {
-        //            prompt: `Generate the names of three subcategories of ${name}. In your response, only provide the name of each broad category, separated by comma. No formatting.`,
-        //            model: 'gpt-3.5-turbo',
-        //            max_tokens: 50,
-        //            n: 1,
-        //            stop: ".",
-        //        },
-        //        {
-        //            headers: {
-        //                "Content-Type": "application/json",
-        //                Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
-        //            }
-        //        }
-        //    );
-        //    console.log(response);
-        //}
-        //async function OpenaiPrompt3(name) {
-        //    const response = await axios.post(
-        //        "https://api.openai.com/v1/completions",
-        //        {
-        //            prompt: `Generate fake product names and prices (in USD) for ${name}. The names should sound like they would appear on an online shopping website. In your response, only provide the name of each item, each on a new line. No formatting.`,
-        //            model: 'gpt-3.5-turbo',
-        //            max_tokens: 1000,
-        //            n: 1,
-        //            stop: ".",
-        //        },
-        //        {
-        //            headers: {
-        //                "Content-Type": "application/json",
-        //                Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
-        //            }
-        //        }
-        //    );
-        //    console.log(response);
-        //}
+        async function OpenaiPrompt2(name) {
+            const response = await axios.post(
+                "https://api.openai.com/v1/completions",
+                {
+                    prompt: `Generate the names of three subcategories of ${name}. Each item should have its first letter capitalized.In your response, only provide the name of each broad category, separated by comma.Do not include any words other than the categories.Example: “Food, Arts and Crafts Materials, Tech Gadgets”`,
+                    model: 'text-davinci-003',
+                    max_tokens: 50,
+                    temperature: 0.5,
+                    n: 1,
+                    stop: ".",
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+                    }
+                }
+            );
+            const cate = response.data.choices[0].text.split(',');
+            setc1(cate[0]);
+            setc2(cate[1]);
+            setc3(cate[2]);
+        }
+        async function OpenaiPrompt3(name) {
+             const response = await axios.post(
+                "https://api.openai.com/v1/completions",
+                {
+                    prompt: `Generate fake product names and prices (in USD) for ${name}. The names should sound like they would appear on an online shopping website. Each item and price pair should be separated by a semicolon. The format should be “[name] - [cost];”. For example: “Sparkling Berry Fusion - $3.99; Premium Green Tea Elixir - $2.49”;`,
+                    model: 'text-davinci-003',
+                    max_tokens: 1000,
+                    temperature: 0.5,
+                    n: 1,
+                    stop: ".",
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+                    }
+                }
+            );
+            console.log(response.data.choices[0].text);
+        }
         OpenaiPrompt();
+        //OpenaiPrompt2('Food');
+        //OpenaiPrompt3('School supplies');
+
     }, []);
 
     
